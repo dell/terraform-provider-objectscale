746a4b28218be32a3897ffb22d2fc5c8ddef9d8a
Rounak Adhikary <Rounak.Adhikary@dell.com>
2025-12-03 08:46:17 -0500

    Adding documentation

--- internal/provider/iam_group_resource.go
+++ internal/provider/iam_group_resource.go
@@ -52,6 +52,8 @@ func (r *IAMGroupResource) Metadata(ctx context.Context, req resource.MetadataRe
 
 func (r *IAMGroupResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
+		Description:         "Manages an ObjectScale IAM Group.",
+		MarkdownDescription: "Manages an ObjectScale IAM Group.",
 		Attributes: map[string]schema.Attribute{
 			"name": schema.StringAttribute{
 				Description:         "Simple name identifying the group. Required",
