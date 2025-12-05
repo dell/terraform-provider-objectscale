cf833daa8602f96976c9c69bc6478620e55a28fd
MangirishK <Mangirish.Kenkare@dell.com>
2025-12-05 04:59:14 -0500

    Adding examples

---
+++ examples/resources/objectscale_iam_managed_policy/import.sh
@@ -0,0 +1,31 @@
+# Copyright (c) 2025 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+# Licensed under the Mozilla Public License Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://mozilla.org/MPL/2.0/
+
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# The command is
+# terraform import objectscale_iam_managed_policy.example <namespace>:<entity_type>:<entity_name>
+# <namespace>: Namespace to which the IAM entity belongs.
+# <entity_type>: One of user, group, or role.
+# <entity_name>: The name of the user, group, or role.
+# Examples:
+# Import managed policies for a user
+# terraform import objectscale_iam_managed_policy.example ns1:user:userTest1
+# Import managed policies for a group
+# terraform import objectscale_iam_managed_policy.example ns1:group:groupTest1
+# Import managed policies for a role
+# terraform import objectscale_iam_managed_policy.example ns1:role:roleTest1
+
+# after running this command, populate the policy_arns field and other required parameters in the config file to start managing this resource.
+# Note: running "terraform show" after importing shows the current config/state of the resource. You can copy/paste that config to make it easier to manage the resource.
---
+++ examples/resources/objectscale_iam_managed_policy/resource.tf
@@ -0,0 +1,35 @@
+/*
+Copyright (c) 2025 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://mozilla.org/MPL/2.0/
+
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+# Available actions: Create, Update, Delete and Import
+# Before running `terraform apply`, ensure that the specified user/group/role exists in the ObjectScale
+# Running `terraform apply` will set the specified managed policies for that user/group/role in the ObjectScale
+resource "objectscale_iam_managed_policy" "example" {
+  # Namespace to which the IAM entity belongs must be provided
+  namespace = "ns1"
+
+  # Exactly one of username, groupname, or rolename must be provided
+  # username = "userTest1"
+  # groupname = "groupTest1"
+  rolename  = "roleTest1"
+
+  # List of managed policies to be set on the specified IAM entity must be provided
+  policy_arns = [
+    "urn:ecs:iam:::policy/ECSS3ReadOnlyAccess",
+    "urn:ecs:iam:::policy/IAMReadOnlyAccess"
+  ]
+}
