81c0df6035bdcbfc7f0fd02827afd48d15a5138a
Aniket Das <aniket.das@dell.com>
2025-12-08 18:12:32 +0530

    Adding support for IAM Group Membership (#20)
    * WIP Commit : IAM Group Membership Resource
    
    * IAM Group Membership Resource CRUD operations completed
    
    * Improving Test Coverage
    
    * Test Coverage Improved : More Mocks
    
    * Addressing Review Comments and minor tweaks
    
    * Added testUserTokenCleanup to tests

---
+++ internal/provider/iam_group_membership_resource_test.go
@@ -0,0 +1,232 @@
+/*
+Copyright (c) 2025 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://mozilla.org/MPL/2.0/
+
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package provider
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"regexp"
+	"terraform-provider-objectscale/internal/clientgen"
+	"testing"
+
+	"github.com/bytedance/mockey"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	resourceconfig "github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+// Test to Fetch Namespaces
+func TestAccIAMGroupMembershipResource(t *testing.T) {
+	if os.Getenv("TF_ACC") == "" {
+		t.Skip("Dont run with units tests because it will try to create the context")
+	}
+	var apiMocker *mockey.Mocker
+	defer testUserTokenCleanup(t)
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+
+		Steps: []resource.TestStep{
+			// invalid config testing
+			{
+				Config: ProviderConfigForTesting + `
+				resource "objectscale_iam_group" "example" {
+					name      = "testacc_group"
+					namespace = "ns1"
+					}
+					
+					resource "objectscale_iam_group_membership" "example_membership" {
+					name      = objectscale_iam_group.example.name
+					namespace = objectscale_iam_group.example.namespace
+					// user is missing
+				}
+				`,
+				ExpectError: regexp.MustCompile(".*The argument \"user\" is required, but no definition was found..*"),
+			},
+			{
+				Config: ProviderConfigForTesting + `
+				resource "objectscale_iam_group" "example" {
+					name      = "testacc_group"
+					namespace = "ns1"
+					}
+					
+					resource "objectscale_iam_group_membership" "example_membership" {
+						name      = objectscale_iam_group.example.name
+						namespace = objectscale_iam_group.example.namespace
+						user      = "testacc-user"
+						}
+						`,
+				ExpectError: regexp.MustCompile(".*NoSuchEntity.*"),
+			},
+			// Create
+			{
+				Config: ProviderConfigForTesting + `
+				resource "objectscale_iam_group" "example" {
+					name      = "testacc_group"
+					namespace = "ns1"
+				}
+		
+				resource "objectscale_iam_group_membership" "example_membership" {
+					name      = objectscale_iam_group.example.name
+					namespace = objectscale_iam_group.example.namespace
+					user      = "test-user"
+				}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("objectscale_iam_group_membership.example_membership", "name", "testacc_group"),
+					resource.TestCheckResourceAttr("objectscale_iam_group_membership.example_membership", "namespace", "ns1"),
+					resource.TestCheckResourceAttr("objectscale_iam_group_membership.example_membership", "user", "test-user"),
+				),
+			},
+			{
+				PreConfig: func() {
+					apiMocker = mockey.Mock((*clientgen.IamApiService).IamServiceGetGroupExecute).
+						Return(mockey.Sequence(
+							&clientgen.IamServiceGetGroupResponse{
+								GetGroupResult: &clientgen.IamServiceGetGroupResponseGetGroupResult{
+									Group: &clientgen.IamServiceCreateGroupResponseCreateGroupResultGroup{
+										GroupId:    getpointer("testacc_group-id"),
+										GroupName:  getpointer("testacc_group"),
+										Arn:        getpointer("arn:aws:iam::123456789012:group/testacc_group"),
+										CreateDate: getpointer("2024-01-01T00:00:00Z"),
+									},
+								},
+							}, nil, nil).Then(nil, nil, fmt.Errorf("error"))).
+						Build()
+				},
+				Config: ProviderConfigForTesting + `
+				resource "objectscale_iam_group" "example" {
+					name      = "testacc_group"
+					namespace = "ns1"
+				}
+		
+				resource "objectscale_iam_group_membership" "example_membership" {
+					name      = objectscale_iam_group.example.name
+					namespace = objectscale_iam_group.example.namespace
+					user      = "test-user"
+				}
+				`,
+				PlanOnly:    true,
+				ExpectError: regexp.MustCompile(".*Error reading Group.*"),
+			},
+			// update not supported test
+			{
+				PreConfig: func() {
+					apiMocker.UnPatch() // cleanup after the previous step
+				},
+				Config: ProviderConfigForTesting + `
+				resource "objectscale_iam_group" "example" {
+					name      = "testacc_group"
+					namespace = "ns1"
+				}
+		
+				resource "objectscale_iam_group_membership" "example_membership" {
+					name      = objectscale_iam_group.example.name
+					namespace = objectscale_iam_group.example.namespace
+					user      = "test-user1"
+				}
+				`,
+				ExpectError: regexp.MustCompile(".*Update operation is not supported.*"),
+			},
+			{
+				// test for import
+				ResourceName: "objectscale_iam_group_membership.example_membership",
+				ImportState:  true,
+				ExpectError:  regexp.MustCompile(".*Import operation is not available.*"),
+			},
+		},
+	})
+}
+
+func TestAccIAMGroupMembershipResource_InvalidAPIClient(t *testing.T) {
+
+	if os.Getenv("TF_ACC") == "" {
+		t.Skip("Dont run with units tests because it will try to create the context")
+	}
+	var apiMocker, api1mocker *mockey.Mocker
+	defer testUserTokenCleanup(t)
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				PreConfig: func() {
+					apiMocker = mockey.Mock((*clientgen.IamApiService).IamServiceAddUserToGroupExecute).
+						Return(nil, nil, fmt.Errorf("error")).
+						Build()
+				},
+				Config: ProviderConfigForTesting + `
+				resource "objectscale_iam_group_membership" "example_membership" {
+					name      = "mocked_group"
+					namespace = "ns1"
+					user      = "mocked_user"
+				}
+				`,
+				ExpectError: regexp.MustCompile(".*Error adding user to group.*"),
+			},
+			{
+				PreConfig: func() {
+					apiMocker.UnPatch() // cleanup after the previous step
+					apiMocker = mockey.Mock((*clientgen.IamApiService).IamServiceAddUserToGroupExecute).
+						Return(nil, nil, nil).
+						Build()
+					api1mocker = mockey.Mock((*clientgen.IamApiService).IamServiceGetGroupExecute).
+						Return(nil, nil, fmt.Errorf("error")).
+						Build()
+				},
+				Config: ProviderConfigForTesting + `
+				resource "objectscale_iam_group_membership" "example_membership" {
+					name      = "mocked_group"
+					namespace = "ns1"
+					user      = "mocked_user"
+				}
+				`,
+				ExpectError: regexp.MustCompile(".*Error reading Group.*"),
+			},
+		},
+	})
+
+	api1mocker.UnPatch()
+	apiMocker.UnPatch()
+}
+
+func TestAccIAMGroupMembershipResource_Configure_InvalidType(t *testing.T) {
+	ctx := context.Background()
+
+	r := &IAMGroupMembershipResource{}
+
+	req := resourceconfig.ConfigureRequest{
+		ProviderData: 12345, // wrong type to trigger the !ok branch
+	}
+	resp := &resourceconfig.ConfigureResponse{
+		Diagnostics: diag.Diagnostics{},
+	}
+
+	r.Configure(ctx, req, resp)
+
+	if !resp.Diagnostics.HasError() {
+		t.Fatalf("expected diagnostics error when ProviderData type is invalid, got none")
+	}
+
+	got := resp.Diagnostics[0].Summary()
+	want := "Unexpected Resource Configure Type"
+	if got != want {
+		t.Errorf("unexpected diagnostic summary: got %q, want %q", got, want)
+	}
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -139,6 +139,7 @@ func (p *ObjectScaleProvider) Resources(ctx context.Context) []func() resource.R
 		NewIAMUserResource,
 		NewIAMInlinePolicyResource,
 		NewIAMGroupResource,
+		NewIAMGroupMembershipResource,
 	}
 }
 
